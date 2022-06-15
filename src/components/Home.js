248be49344f1b5aad44eb2f402ec27f51aac0ecf
amit-kumar56 <amit10041999@gmail.com>
2022-06-16 00:25:30 +0530

    project created

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home=()=>{
+    return(
+        <div className="nav active ">
+            <div className='display-4 text-center'>This is your home page. Description your self here.</div>
+        </div>
+    )
+}
+
+export default Home;
+
